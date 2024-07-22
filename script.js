03bc88029457c6d4b64ffa095b2409c1b5fc7f45
Aidan Maldonado <aidanmaldonado@aidans-air.lan>
2024-07-22 04:39:15 -0700

    Stiched backend to frontend

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 // });
 
 
+
 document.getElementById('grid_container').addEventListener('submit', function(e) {
     e.preventDefault(); // Prevent the default form submission
   
@@ -16,7 +17,7 @@ document.getElementById('grid_container').addEventListener('submit', function(e)
   
     console.log("Submitting form data...");
   
-    fetch('https://google.com', {
+    fetch('http://127.0.0.1:5000/process', {
       method: 'POST',
       body: formData
     })
@@ -27,6 +28,42 @@ document.getElementById('grid_container').addEventListener('submit', function(e)
       console.log("Form submitted successfully");
       document.getElementById('ticker_input').value = ''; // Clear form inputs if needed
       document.getElementById('margin_input').value = '';
+      return response.json(); // Parse the JSON from the response
     })
-    .catch(error => console.error('Fetch error:', error)); // Log any fetch errors to the console
+    .then(data => {
+      var stockImage = document.getElementById('photo');
+      console.log("DATA RECEIVED:", data);
+      // Find the image with the src "Prediction.png"
+      console.log("ye");
+      // Create a new image element
+      const newImage = new Image();
+      newImage.src = 'plot.png?' + new Date().getTime();
+      // stockImage.style.transition = 'opacity 0.1s ease-in-out, transform 0.1s ease-in-out';
+      // stockImage.style.opacity = 0;
+      // stockImage.style.transform = 'scale(0.8)';
+  
+      newImage.onload = () => {
+        // Fade out the current image
+  
+        // Wait for the fade-out to complete, then change the src and fade in
+        setTimeout(() => {
+          stockImage.src = newImage.src; // Update the existing image src once the new image is loaded
+          stockImage.style.transition = 'opacity 0.1s ease-in, transform 0.3s ease-in';
+          stockImage.style.opacity = 1; // Fade in
+          stockImage.style.transform = 'scale(1)'; // Scale to original size
+        }, 0); // 0.08s delay to match the fade-out duration
+      }
+    })
+    .catch(error => console.error('Fetch bad error:', error)); // Log any fetch errors to the console
   });
+  
+  
+  const runner = document.getElementById("run_button");
+  runner.onclick = function() {
+      console.log("disappear");
+      var stockImage = document.getElementById('photo');
+      stockImage.style.transition = 'opacity 0.15s ease-in-out, transform 0.15s ease-in-out';
+      stockImage.style.opacity = 0;
+      stockImage.style.transform = 'scale(0.95)';
+  };
+  
